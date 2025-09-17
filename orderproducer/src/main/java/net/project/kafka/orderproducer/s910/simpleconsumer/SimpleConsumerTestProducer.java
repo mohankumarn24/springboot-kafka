bf029b4e19bdf0c8cbf47b6936b69ce38b4973c9
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-09-17 18:48:31 +0530

    add simple consumer notes

---
+++ orderproducer/src/main/java/net/project/kafka/orderproducer/s910/simpleconsumer/SimpleConsumerTestProducer.java
@@ -0,0 +1,28 @@
+package net.project.kafka.orderproducer.s910.simpleconsumer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import net.project.kafka.orderproducer.s4.producerconsumer.OrderCallBack;
+import java.util.Properties;
+
+public class SimpleConsumerTestProducer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerSerializer");
+
+        KafkaProducer<String, Integer> producer = new KafkaProducer<>(props);
+        ProducerRecord<String, Integer> record = new ProducerRecord<>("SimpleConsumerTopic", "Mac Book Pro", 10);
+
+        try {
+            producer.send(record,new OrderCallBack());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+    }
+}
