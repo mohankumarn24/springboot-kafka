452f04f9b4ce62e1af705ec745ef00cb22f9e970
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-09-17 21:12:08 +0530

    refactor code

--- userproducer/src/main/java/net/project/kafka/userproducer/controller/UserController.java
+++ userproducer/src/main/java/net/project/kafka/userproducer/controller/UserController.java
@@ -20,3 +20,16 @@ public class UserController {
         service.sendUserData(user);
     }
 }
+
+/*
+cd C:\dev\kafka_2.12-3.4.0
+.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
+.\bin\windows\kafka-server-start.bat .\config\server.properties
+
+
+POST: http://localhost:8080/api/v1/users/publish
+{
+  "name" : "Mohan",
+  "genre" : "Action"
+}
+ */
