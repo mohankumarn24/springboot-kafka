d69ccd5bed9aa4e81e7787b35dca6e1a48548417
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-09-17 16:40:21 +0530

    refactor code

---
+++ orderconsumer/src/main/java/net/project/kafka/orderconsumer/s7/partitioners/OrderConsumerProps.java
@@ -0,0 +1,30 @@
+package net.project.kafka.orderconsumer.s7.partitioners;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class OrderConsumerProps {
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "OrderGroup");
+
+        KafkaConsumer<String, Integer> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("OrderTopicProps"));
+
+        ConsumerRecords<String, Integer> records = consumer.poll(Duration.ofSeconds(20));
+        for (ConsumerRecord<String, Integer> record: records) {
+            System.out.println(String.format("ProductName=%s, Quantity=%d", record.key(), record.value()));
+        }
+        consumer.close();
+    }
+}
